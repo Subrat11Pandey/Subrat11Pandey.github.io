5fcf0bd53718d3ab7caf1e5e78b94206c4855a4a
Subrat Pandey <77990271+Subrat11Pandey@users.noreply.github.com>
2023-07-11 12:03:07 +0530

    Update webapp.py

--- Web_GUI/webapp.py
+++ Web_GUI/webapp.py
@@ -1,6 +1,8 @@
 import torch
+import pygame
+import tensorflow as tf
 import argparse
-import cv2 , numpy as np 
+import cv2 , numpy as np , pandas as pd
 import tensorflow as tf
 from re import DEBUG ,sub
 from flask import Flask, render_template , request , redirect, send_file , url_for, Response
@@ -10,6 +12,7 @@ from subprocess import Popen
 import re , requests ,shutil , time , glob
 from PIL import Image
 from ultralytics import YOLO
+from keras.preprocessing.image import ImageDataGenerator
 import io
 import base64
 app = Flask(__name__  )
@@ -57,7 +60,7 @@ def get_frame():
         ret, jpgg = cv2.imencode('.jpg', image)
         yield(b'--frame\r\n'
                 b'Content-Type: image/jpgg\r\n\r\n' + jpgg.tobytes() + b'r\n\r\n')
-        time.sleep(0,1)
+        # time.sleep(0,1)
 
 @app.route("/video_feed")
 def video_feed():
@@ -66,6 +69,9 @@ def video_feed():
 
 @app.route("/", methods=["GET", "POST"])
 def predict_img():
+    pygame.init()
+    
+
     if request.method == 'POST':
         if 'file' in request.files:
             f = request.files['file']
@@ -86,12 +92,52 @@ def predict_img():
                 img = cv2.imread(filepath)
                 _, frame = cv2.imencode('.jpg', img)
                 image = Image.open(io.BytesIO(frame))
+# increasing the quality of image
+                datagen = ImageDataGenerator(brightness_range=[0.5,2.0])
+                for x in range(image.size[0]):
+                    for y in range(image.size[1]):
+                        r, g, b = image.getpixel((x, y))
+                        image.putpixel((x, y), (r, g, 0))
+
                 yolo = YOLO('bestv2.pt')
                 print("detection about to start")
                 print(f.filename)
-                print(type(image))
+                # print(type(image))
                 detections = yolo.predict(filepath ,save = True )
-                return display(f.filename)
+
+                # Assuming detection[0] is the Results object
+                results = detections[0]  # Assuming detection[0] is the Results object
+                class_names  = results.probs # Get the dictionary of class names
+                class_ = results.boxes.cls
+                if results.boxes.cls.size(0) != 0:
+                    for value in class_:
+                        value = int(value.item())
+                
+                    if value == 0:
+                        print("Speed Breaker")
+                        pygame.mixer.music.load('breaker.wav')
+                        pygame.mixer.music.play()
+                    elif value == 1:
+                        print("crosswalk")
+                        pygame.mixer.music.load('speed.mp3')
+                        pygame.mixer.music.play()
+                    elif value == 3:
+                        print("speedlimit")
+                        pygame.mixer.music.load('speed.mp3')
+                        pygame.mixer.music.play()
+                    elif value == 4:
+                        print("stop")
+                        pygame.mixer.music.load('stop_sound.wav')
+                        pygame.mixer.music.play()
+
+                    elif value == 5:
+                        print("trafficlight")
+                    return display(f.filename)
+                else:
+                    return display(f.filename)
+                print("")
+                print("")
+                
             elif file_extension == 'mp4':
                 video_path  = filepath
                 cap = cv2.VideoCapture(video_path)
@@ -102,24 +148,27 @@ def predict_img():
                 out = cv2.VideoWriter('output.mp4' , fourcc, 30.0,(frame_width,frame_height))
 
                 model = YOLO('bestv2.pt')
-
+                frame_counter = 0
                 while cap.isOpened():
                     ret , frame = cap.read()
                     if not ret:
                         break
-                    results = model(frame , save= True)
-                    print(results)
-                    cv2.waitKey(1)
+                    
+                    if frame_counter % 8 == 0:
+                        results = model(frame , save= True)
+                        print(results)
+                        cv2.waitKey(1)
 
-                    res_plotted = results[0].plot()
-                    cv2.imshow("result", res_plotted)
-
+                        res_plotted = results[0].plot()
+                        cv2.imshow("result", res_plotted)
+                    frame_counter +=1
                     out.write(res_plotted)
 
 
                     if cv2.waitKey(1) == ord('q'):
                         break
                 return video_feed()
+
 
     
 
@@ -131,4 +180,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     # model = torch.hub.load('.', 'custom','best.pt', source='local')
     # model.eval()
-    app.run(host="127.0.0.1", port=args.port , debug = True) 
+    app.run(host="127.0.0.1", port=args.port , debug = False) 
